2e644f9e2f45f1b591af59e87708ef582b649689
Andrés Díaz de León <andruix93@gmail.com>
2021-08-17 21:52:07 +0000

    test lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
+func getName() error{
+	if len(os.Args) > 1{
+		for i, val := range os.Args {
+			if i == 0{
+				continue
+			}
+			fmt.Printf("%s ", string(val))
+		}
+		fmt.Print("Welcome to the jungle\n")
+	}else{
+		return errors.New("no argument given")
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("Welcome to the jungle")
+	err := getName()
+	if err != nil{
+	}
 }
