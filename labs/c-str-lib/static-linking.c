e9fe7d030b3d450dae4988ed4b786bdfa901e4c8
Andrés Díaz de León <andruix93@gmail.com>
2021-09-30 04:38:06 +0000

    submiting lab

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if(argc < 4){
+        return 0;
+    }
+
+    if(!strcmp(argv[1], "-add")){
+        char *q = NULL;
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        q = mystradd(argv[2], argv[3]);
+        printf("New String          : %s\n", q);
+        printf("New length          : %d\n", mystrlen(q));
+    }else if(!strcmp(argv[1], "-find")){
+        int pos = mystrfind(argv[2], argv[3]);
+        printf("[%s] string was found at [%d] position\n", argv[2], pos);
+    }
+
+    
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,60 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int len=0;
+    while(*str!='\0')
+    {
+        len++;
+        *str++;
+    }
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *q = NULL;
+    int len1 = mystrlen(origin);
+    int len2 = mystrlen(addition);
+    q = (char *)malloc(len1 + len2 + 1);
+    for (int i = 0; i < mystrlen(origin); i++)
+    {
+        q[i] = origin[i]; 
+    }
+    
+    for (int i =  0; i < len2; i++)
+    {
+        q[i+len1] = addition[i];
+    }
+    q[len1 + len2 + 1] = '\0';
+    
+    return q;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int i=0;
+    int len = mystrlen(substr);
+    int pos = 0;
+    int firstletter = 0;
+    while(*origin!='\0')
+    {
+        if(substr[i] == *origin){
+            if(i == 0){
+                firstletter = pos;
+            }
+            if (i == len-1){
+                return firstletter;
+            }
+            i++;
+        }else{
+            i = 0;
+        }
+
+        if(*origin == ' '){
+            pos++;
+        }
+
+        *origin++;
+    }
+
+    return -1;
 }
